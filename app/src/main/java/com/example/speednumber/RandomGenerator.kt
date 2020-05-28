5bc538acc198a7229cfb5834f256cbab249e630e
AxyseCom <axyse.com@gmail.com>
2020-05-28 21:21:49 +0300

    first web commit

--- app/src/main/java/com/example/speednumber/RandomGenerator.kt
+++ app/src/main/java/com/example/speednumber/RandomGenerator.kt
@@ -2,8 +2,6 @@ package com.example.speednumber
 
 class RandomGenerator(val a: Int, val b: Int) {
 
-    var asd = 11
-
     fun getLeftNumber() : Int {
         val random = (a..b).random()
         return random
