3a072474ea425ca7bd64a3ab9f8a95c65d1cfff4
AxyseCom <axyse.com@gmail.com>
2020-06-06 22:13:04 +0300

    database helper commit

---
+++ app/src/main/java/com/example/speednumber/DataBaseHelper.kt
@@ -0,0 +1,32 @@
+package com.example.speednumber
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context, name: String, record: Int):SQLiteOpenHelper(context, DATABASE_RECORD, null, DB_VERSION) {
+
+    companion object {
+        private val DATABASE_RECORD = "userstore.db" // название бд
+        private val DB_VERSION = 1 // версия базы данных
+        internal val TABLE = "users" // название таблицы в бд
+        // названия столбцов
+        val COLUMN_ID = "_id"
+        val COLUMN_RECORD = "record"
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(("CREATE TABLE users (" + COLUMN_ID
+                + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_RECORD + " INTEGER);"))
+        // добавление начальных данных
+        db.execSQL(("INSERT INTO " + TABLE + " (" + COLUMN_RECORD
+                + ") VALUES ('', 0);"))
+    }
+
+    override fun onUpgrade(db:SQLiteDatabase, oldVersion:Int, newVersion:Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE)
+        onCreate(db)
+    }
+
+
+}
